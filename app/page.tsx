32abd8f7106004b260c37d92dde4531ca92203f3
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2023-05-30 08:16:05 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
       <h1>Hello World !</h1>
+      <p>Ici un paragraphe</p>
     </main>
-  )
+  );
 }
